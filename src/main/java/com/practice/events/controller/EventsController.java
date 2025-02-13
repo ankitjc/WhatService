fa451b12a057ebe9619a8b1e206e98ab082989ab
ankitjc <ankitjc@gmail.com>
2025-02-13 13:48:27 -0500

    Adding @CrossOrigin

--- src/main/java/com/practice/events/controller/EventsController.java
+++ src/main/java/com/practice/events/controller/EventsController.java
@@ -2,11 +2,13 @@ package com.practice.events.controller;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.practice.events.model.Event;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class EventsController {
 
     private static final String template = "Hello, %s!";
