a4844a81dcc33ff46d08934303014e6a649b2ab1
ankitjc <ankitjc@gmail.com>
2025-02-18 10:53:38 -0500

    swagger-ui

--- src/main/java/com/practice/events/EventsApplication.java
+++ src/main/java/com/practice/events/EventsApplication.java
@@ -2,8 +2,10 @@ package com.practice.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class EventsApplication {
 
 	public static void main(String[] args) {
