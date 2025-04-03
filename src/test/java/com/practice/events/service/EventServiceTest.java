403f77ddaddbdd182adb3a9892150fa06c00f191
ankitjc <ankitjc@gmail.com>
2025-04-02 22:04:58 -0400

    Service test skeleton

---
+++ src/test/java/com/practice/events/service/EventServiceTest.java
@@ -0,0 +1,28 @@
+package com.practice.events.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventServiceTest {
+
+    @Test
+    void getAllAbbreviations() {
+    }
+
+    @Test
+    void getAbbreviationsFor() {
+    }
+
+    @Test
+    void getLeaderboard() {
+    }
+
+    @Test
+    void updateOrInsertUsingRepository() {
+    }
+
+    @Test
+    void deleteAbbreviation() {
+    }
+}
