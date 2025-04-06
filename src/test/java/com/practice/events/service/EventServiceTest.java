a17532c3b72805c0c8aebe4a9b46397f2f65cade
ankitjc <ankitjc@gmail.com>
2025-04-05 20:00:35 -0400

    Adding event service tests

--- src/test/java/com/practice/events/service/EventServiceTest.java
+++ src/test/java/com/practice/events/service/EventServiceTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -30,19 +32,30 @@ class EventServiceTest {
         //Sample data
         Date addedAt = new Date();
         Date lastAccessed = new Date();
-        Abbreviation abbr1 = new Abbreviation(1, "", "", "", false, "", addedAt, lastAccessed);
-        Abbreviation abbr2 = new Abbreviation();
+        Abbreviation abbr1 = new Abbreviation(1, "SF1", "", "", false, "", addedAt, lastAccessed);
+        Abbreviation abbr2 = new Abbreviation(2, "SF2", "", "", false, "", addedAt, lastAccessed);
 
         mockResult = Lists.list(abbr1, abbr2);
     }
 
     @Test
-    void getAllAbbreviations() {
-        when(mockRepo.findAll()).thenReturn(mockResult);
-
-        Page<Abbreviation> actualResponse = mockService.getAllAbbreviations(1, 10, "", false);
+    void getAllAbbreviationsSortedInAscendingOrder() {
+        Page<Abbreviation> pageResult = new PageImpl<>(mockResult);
+        when(mockRepo.findAll((Pageable) any())).thenReturn(pageResult);
+        Page<Abbreviation> actualResponse = mockService.getAllAbbreviations(1, 10, "shortForm", false);
 
         assertEquals(mockResult.size(), actualResponse.getTotalElements());
+        assertEquals("SF1", actualResponse.getContent().get(0).getShortForm());
+    }
+
+    @Test
+    void getAllAbbreviationsSortedInDescendingOrder() {
+        Page<Abbreviation> pageResult = new PageImpl<>(mockResult);
+        when(mockRepo.findAll((Pageable) any())).thenReturn(pageResult);
+        Page<Abbreviation> actualResponse = mockService.getAllAbbreviations(1, 10, "shortForm", true);
+
+        assertEquals(mockResult.size(), actualResponse.getTotalElements());
+        assertEquals("SF2", actualResponse.getContent().get(1).getShortForm());
     }
 
     @Test
